8f60fa27203ef3929e7a0b1ae5c29559f293f0af
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-05-30 23:01:58 +0200

    Add LSP

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -53,8 +53,12 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp'
   use 'saadparwaiz1/cmp_luasnip'
-  ---- snippets
+  -- snippets
   use 'L3MON4D3/LuaSnip'
   use 'rafamadriz/friendly-snippets'
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
 end)
