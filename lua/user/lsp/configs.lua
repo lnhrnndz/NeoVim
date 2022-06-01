7ac12c34bc4f14b7077db9d47839d11fcac41fef
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-06-01 16:38:54 +0200

    Update LSP

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua" }
+local servers = { "jsonls", "sumneko_lua", "csharp_ls", "bashls", "pyright", "clangd" }
 
 lsp_installer.setup {
 	ensure_installed = servers
