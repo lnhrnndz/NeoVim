7da2e57c596da2a72671188634bf75f36d05abe7
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-06-09 09:40:03 +0200

    Update lualine

--- lua/stuff/lualine.lua
+++ lua/stuff/lualine.lua
@@ -33,8 +33,46 @@ require('lualine').setup {
             -- or nil on failure. count <= 0 won't be displayed.
             padding = { left = 0, right = 1 }
         },
-        { 'diagnostics' },
-        { 'filename' },
+        {
+            'diagnostics',
+
+            -- Table of diagnostic sources, available sources are:
+            --   'nvim_lsp', 'nvim_diagnostic', 'coc', 'ale', 'vim_lsp'.
+            -- or a function that returns a table as such:
+            --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+            sources = { 'nvim_lsp' },
+
+            -- Displays diagnostics for the defined severity types
+            sections = { 'error', 'warn', 'info', 'hint' },
+
+            diagnostics_color = {
+                -- Same values as the general color option can be used here.
+                error = 'DiagnosticError', -- Changes diagnostics' error color.
+                warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+            },
+            symbols = {error = 'E', warn = 'W', info = 'i', hint = 'H'},
+            colored = true,           -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = false,   -- Show diagnostics even if there are none.
+        },
+        {
+            'filename',
+            file_status = true,      -- Displays file status (readonly status, modified status)
+            path = 0,                -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+
+            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+                modified = '[+]',      -- Text to show when the file is modified.
+                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+            }
+        }
     },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
