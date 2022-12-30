fc08a01ab4aa8188203fde383e704b7f2429dc65
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-12-30 09:51:37 +0100

    telescope: ignore .git

--- lua/stuff/telescope.lua
+++ lua/stuff/telescope.lua
@@ -89,6 +89,7 @@ telescope.setup {
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
   },
+  file_ignore_patterns = {".git/"},
   extensions = {
     -- Your extension configuration goes here:
     -- extension_name = {
